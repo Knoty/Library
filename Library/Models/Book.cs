337810be6aff5d73c3ad9205408290f54f5006d2
Knoty <Knoty01@gmail.com>
2020-02-01 10:35:36 +0300

    Добавлены модели сущностей

---
+++ Library/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string BookType { get; set; }
+        public int? WithClientId { get; set; }
+    }
+}
---
+++ Library/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public int? TakenBookId { get; set; }
+    }
+}
