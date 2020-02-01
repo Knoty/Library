26deaa203272a74a9c653c6262dbdc230c1ddd87
Knoty <Knoty01@gmail.com>
2020-02-01 18:31:51 +0300

    Добавлены контроллер и представления

---
+++ Library/Controllers/LibraryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Books()
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book() { BookId = 1, BookName = "Чистый код", BookType = "Учебная" });
+            books.Add(new Book() { BookId = 2, BookName = "Гиперион", BookType = "Развлекательная" });
+            ViewBag.Books = books;
+            ViewData["Message"] = "Список Книг";
+
+            return View();
+        }
+        public IActionResult Clients()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client() { ClientId = 1, ClientName = "Иванов Иван Иванович" });
+            clients.Add(new Client() { ClientId = 2, ClientName = "Петров Петр Петрович" });
+            ViewBag.Clients = clients;
+            ViewData["Message"] = "Список клиентов библиотеки";
+
+            return View();
+        }
+    }
+}
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -56,7 +56,7 @@ namespace Library
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Library}/{action=Index}/{id?}");
             });
         }
     }
